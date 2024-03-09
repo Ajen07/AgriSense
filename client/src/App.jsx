7f240298c810a6e691bc15b26b5b73823a58649b
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-03-09 21:29:27 +0530

    route added for login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
 import Register from "./pages/authentication/Register";
+import Login from "./pages/authentication/Login";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
