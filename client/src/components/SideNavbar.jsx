db846ce228831db2b45a212364d317409eb3272f
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-03-10 22:12:18 +0530

    SideNavbar added

---
+++ client/src/components/SideNavbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Navitem from "./Navitem";
+import overviewLogo from "../assets/overview.png";
+import sensorsLogo from "../assets/sensors.png";
+import settingsLogo from "../assets/settings.png";
+import analyticsLogo from "../assets/analytics.png";
+const SideNavbar = () => {
+  return (
+    <section className="bg-[#c1ffa7] md:flex md:w-[200px] md:justify-center hidden">
+      <nav className="mt-8">
+        <ul className="flex flex-col gap-y-6">
+          <Navitem
+            to="/"
+            image={overviewLogo}
+            altText="Review"
+            name="Review"
+          />
+          <Navitem
+            to="sensors"
+            image={sensorsLogo}
+            altText="sensors"
+            name="Sensors"
+          />
+          <Navitem
+            to="settingsLogo"
+            image={settingsLogo}
+            altText="settings"
+            name="Settings"
+          />
+          <Navitem
+            to="analytics"
+            image={analyticsLogo}
+            altText="Analytics"
+            name="Analytics"
+          />
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default SideNavbar;
